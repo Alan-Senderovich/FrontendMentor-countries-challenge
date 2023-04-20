237670e8e3717d17831263b1492d441e10dc8c44
Alan Senderovich <69826656+Alan-Senderovich@users.noreply.github.com>
2023-04-20 17:07:05 -0300

    Card and Country Page texts changed

--- src/components/SingleCountryCard.tsx
+++ src/components/SingleCountryCard.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 
 const SingleCountryCard = ({ flags, currencies, name, population, region, subregion, tld, languages, borders, capital }: SingleCountryItemTS) => {
 
+    const formattedPopulation = population.toLocaleString('en-US');
+
     return (
         <div className='tracking-wide grid grid-cols-1 justify-items-center md:grid-cols-2 md:gap-16'>
             <div className='mb-12 w-full'>
@@ -14,15 +16,15 @@ const SingleCountryCard = ({ flags, currencies, name, population, region, subreg
                     <h1 className='text-[1.7rem] font-bold pb-8'>{name}</h1>
                     <div className='flex flex-col md:flex-row md:gap-10'>
                         <div className='flex flex-col gap-2 pb-8'>
-                            <p className='spacing'>Population: <span className='opacity-70'>{population}</span></p>
-                            <p>Region: {region}</p>
-                            <p>SubRegion: {subregion}</p>
-                            <p>Capital: {capital}</p>
+                            <p className='spacing'>Population: <span className='opacity-60'>{formattedPopulation}</span></p>
+                            <p>Region: <span className='opacity-60'>{region}</span></p>
+                            <p>SubRegion: <span className='opacity-60'>{subregion}</span></p>
+                            <p>Capital: <span className='opacity-60'>{capital}</span></p>
                         </div>
                         <div className='flex flex-col gap-2 pb-8'>
-                            <p>Top level domain: {tld}</p>
-                            <p><span>Currencies: </span>{currencies}</p>
-                            <p><span>Languages: </span>{languages}</p>
+                            <p>Top level domain: <span className='opacity-60'>{tld}</span></p>
+                            <p>Currencies: <span className='opacity-60'>{currencies}</span></p>
+                            <p>Languages: <span className='opacity-60'>{languages}</span></p>
                         </div>
                     </div>
                 </div>
@@ -30,7 +32,7 @@ const SingleCountryCard = ({ flags, currencies, name, population, region, subreg
                     <p className='font-bold text-lg pb-4'>Border Countries:</p>
                     <div className='flex flex-wrap'>
                         {borders?.map((border, index) => (
-                            <Link href={`/code/${border}`} className='px-5 py-2 mr-2 mb-2 text-sm font-medium leading-5 text-[#2b3743] bg-white dark:bg-[#2b3743] dark:text-white rounded-sm hover:bg-gray-300 transform transition duration-300 hover:scale-105 active:scale-95 focus:outline-none' key={index}>
+                            <Link href={`/code/${border}`} className='px-5 py-2 mr-2 mb-2 text-sm font-medium leading-5 text-[#2b3743] bg-white dark:bg-[#2b3743] rounded-sm hover:bg-gray-300 transform transition duration-300 hover:scale-105 active:scale-95 focus:outline-none' key={index}>
                                 {border}
                             </Link>
                         ))}
